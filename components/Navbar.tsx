1aa59bedff2a894af2af5334ba82fc4a6001fb96
Veronica Rojas <ar_veronica@yahoo.com>
2023-12-04 17:33:19 +1000

    create constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image"
 import Link from "next/link"
 
 
 export const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flex-between 
+    <nav className="flex-between 
     max-container padding-container relative z-30 py-5">
       <Link
       href="/">
-        
+        <Image
+        src='/hilink-logo.svg'
+        width={74}
+        height={29}
+        alt="hilink logo" />
+
+        <ul className="hidden h-full gap-12 lg:flex">
+
+        </ul>
+
       </Link>
     </nav>
   )

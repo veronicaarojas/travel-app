7038892b5250b65c09f10048fdfe268c9c8dd00c
Veronica Rojas <ar_veronica@yahoo.com>
2023-12-04 17:17:37 +1000

    add components to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -15,7 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
+        <main>
         {children}
+        </main>
+        <Footer />
       </body>
     </html>
   )

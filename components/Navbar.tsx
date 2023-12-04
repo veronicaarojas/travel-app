748ec2f1864e70df0c58cb4fccecf3d051c868c3
Veronica Rojas <ar_veronica@yahoo.com>
2023-12-04 17:27:10 +1000

    add public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
 
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="border-2 border-red-500 flex-between 
+    max-container padding-container relative z-30 py-5">
+      <Link
+      href="/">
+        
+      </Link>
+    </nav>
   )
 }

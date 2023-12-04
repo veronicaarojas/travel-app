739de1a034ad0ef832882d72916a875eb3d01dfd
Veronica Rojas <ar_veronica@yahoo.com>
2023-12-04 17:13:59 +1000

    create navbar component and remove any unecessary code from layout file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500'>
+    <h1 className='font-bold text-teal-500 text-2xl'>
       Hello World
     </h1>
   )
